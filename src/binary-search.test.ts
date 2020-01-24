d78b2ebf08e4dfd28978d463d6507bff3b8495ff
Otis0620 <sharding1012@gmail.com>
2020-01-23 16:45:05 -0800

    correct mistakes in binarySearch and add more tests

--- src/binary-search.test.ts
+++ src/binary-search.test.ts
@@ -1,10 +1,17 @@
 import { binarySearch } from './binary-search';
 
 describe('binarySearch', () => {
-  const items = [1, 3, 2];
+  const items = [1, 2, 3];
+  it('should return true if the number 1 is in the array', () => {
+    expect(binarySearch(1, items)).toBeTruthy();
+  });
 
   it('should return true if the number 2 is in the array', () => {
     expect(binarySearch(2, items)).toBeTruthy();
+  });
+
+  it('should return true if the number 3 is in the array', () => {
+    expect(binarySearch(3, items)).toBeTruthy();
   });
 
   it('should return false if the number 5 is not in the array', () => {
--- src/binary-search.ts
+++ src/binary-search.ts
@@ -2,23 +2,18 @@
  * Search an array for a given value.
  **/
 export function binarySearch(item: number, items: number[]): boolean {
-  if (items[0] === item) {
-    return true;
-  }
-
-  const sortedItems = items.sort((a, b) => b - a);
-  let end = sortedItems.length - 1;
+  let end = items.length - 1;
   let start = 0;
 
-  while (start < end) {
-    let middle = Math.floor((end + start) / 2);
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
 
-    if (sortedItems[middle] === item) {
+    if (items[middle] === item) {
       return true;
-    } else if (sortedItems[middle] < item) {
-      end = middle - 1;
-    } else {
+    } else if (items[middle] < item) {
       start = middle + 1;
+    } else {
+      end = middle - 1;
     }
   }
 
